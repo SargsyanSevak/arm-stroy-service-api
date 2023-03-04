f892e57ea4de1a4b1a248666a3124af7a7a8d000
sargsyansevak <thesevaksargsyan@gmail.com>
2023-03-04 23:43:25 +0400

    strict

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
-
+mongoose.set('strictQuery', false);
 // connect to db
 mongoose.connect(process.env.MONGO)
   .then(() => {
